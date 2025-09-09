74bcec8972ce1ac3a8db8fd72fa112898b15a452
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-09 12:13:19 +0330

    js

---
+++ script.js
@@ -0,0 +1,52 @@
+let optionButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+
+let fontName = document.getElementById("fontName");
+let fontSizeref = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButton = document.querySelectorAll(".script");
+
+//list of fontlist
+let fontList = [
+  "Arial",
+  "Verdana",
+  "Times New Roman",
+  "Garamond",
+  "Georgia",
+  "Courier New",
+  "cursive",
+];
+
+//Initial Settings
+const initializer = () => {
+  //function calls for highlighting buttons
+  highlighter(alignButtons, true);
+  highlighter(spacingButtons, true);
+  highlighter(formatButtons, false);
+  highlighter(scriptButton, true);
+};
+
+//highlight clicked button
+const highlighter = (className, needsRemoval) => {
+  className.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (needsRemoval) {
+        let alreadyActive = false;
+        if (button.classList.contains("active")) {
+          alreadyActive = true;
+        }
+        highlighterRemover(className);
+        if(!alreadyActive){
+            button.classList.add("active");
+        }
+      }
+      else{
+        button.classList.toggle("active");
+      }
+    });
+  });
+};
